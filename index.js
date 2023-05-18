12a43a108ec09172976522a1ab105b86d08ebd92
brett shulman <bshulman810@gmail.com>
2023-05-18 12:35:33 -0400

    added html

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ console.log("hello world");
 
 var proBtn = document.getElementById("project-Btn");
 
-proBtn.addEventListener("click", function(){
-document.location.href ="resume.html"
+proBtn.addEventListener("click", function () {
+  document.location.href = "resume.html";
+});
+
+var chatBtn = document.getElementById("chat-Btn");
+
+chatBtn.addEventListener("click", function () {
+  document.location.href = "case.html";
 });
